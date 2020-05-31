be95425a2e28c4688d3c234f4eabfa3d03ce3d49
unknown <fellypsantos2011@gmail.com>
2020-05-30 21:46:07 -0300

    Methods to read and write Int32 and Float values.

--- WMemory.cs
+++ WMemory.cs
@@ -19,6 +19,15 @@ namespace MemoryHandler
         );
 
         [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int nSize,
+            out IntPtr lpNumberOfBytesRead
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool WriteProcessMemory(
           IntPtr hProcess,
           IntPtr lpBaseAddress,
@@ -55,6 +64,45 @@ namespace MemoryHandler
             IntPtr finalAddress = baseAddress + pOffset;
             WriteProcessMemory(processHandler, finalAddress, bytes, bytes.Length, out bytesWritten);
             return bytesWritten > 0;
+        }
+
+        public Int32 ReadInt32(IntPtr address)
+        {
+            IntPtr bytesRead;
+            byte[] buffer = new byte[4];
+            ReadProcessMemory(processHandler, address, buffer, buffer.Length, out bytesRead);
+
+            return BitConverter.ToInt32(buffer, 0);
+        }
+
+        public float ReadFloat(IntPtr address)
+        {
+            IntPtr bytesRead;
+            byte[] buffer = new byte[4];
+            ReadProcessMemory(processHandler, address, buffer, buffer.Length, out bytesRead);
+
+            return BitConverter.ToSingle(buffer, 0);
+        }
+
+        public bool WriteFloat(IntPtr address, float value)
+        {
+            byte[] floatBytes = BitConverter.GetBytes(value);
+            int bytesWritten = 0;
+
+            WriteProcessMemory(processHandler, address, floatBytes, floatBytes.Length, out bytesWritten);
+            return bytesWritten > 0;
+        }
+
+        public long getFinalAddressFromOffset(int startAddress, int[] offsets)
+        {
+            long address = (baseAddress + startAddress).ToInt32();
+
+            foreach (int offset in offsets)
+            {
+                address = ReadInt32((IntPtr)address) + offset;
+            }
+
+            return address;
         }
     }
 }
